9ebffbaa2a826b7d9ea3e1a318890def762d486b
Yashodhan Gurav <guravyashodhan@gmail.com>
2026-05-24 18:51:45 +0530

    Time: 10 ms (6.90%) | Memory: 28.4 MB (7.67%) - LeetSync

--- 169-majority-element/majority-element.cpp
+++ 169-majority-element/majority-element.cpp
@@ -1,16 +1,22 @@
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
-        int candidate = 0, count = 0;
+        unordered_map<int, int> m;
 
-        for (int num : nums) {
-            if (count == 0) {
-                candidate = num;
-            }
-
-            count += (num == candidate) ? 1 : -1;
+   for(int i = 0; i<nums.size(); i++){
+        if(m.find(nums[i]) != m.end()){
+            m[nums[i]]++;                         //if the element is already present in the map then its freq value will be increased by 1
+        }else{
+            m[nums[i]] = 1; //if the element is not present in the map then it will be added with freq val 1
         }
+    }
+                               
 
-        return candidate;
+   for(pair<int, int> ele : m){
+        if(ele.second > nums.size()/2){
+            return ele.first;
+        }
+   }
+  return -1;
     }
 };
