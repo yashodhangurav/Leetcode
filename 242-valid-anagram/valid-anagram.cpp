4b27fba03f655d3e00b848a6ef1c912f34064c40
Yashodhan Gurav <guravyashodhan@gmail.com>
2026-07-22 19:20:00 +0530

    Time: 4 ms (34.70%) | Memory: 9.8 MB (26.07%) - LeetSync

--- 242-valid-anagram/valid-anagram.cpp
+++ 242-valid-anagram/valid-anagram.cpp
@@ -1,23 +1,30 @@
 class Solution {
 public:
-
     bool isAnagram(string s, string t) {
-        if(s.length() != t.length() ){
+        if(s.size() != t.size()){
             return false;
         }
 
-        int count[26] = {0};
+        unordered_map<char,int> frq;
 
-        for(int i = 0; i<s.length(); i++){
-            count[s[i] - 'a']++;
-        }
-
-        for(int i = 0; i< t.length(); i++){
-            if(count[t[i] - 'a'] == 0){
-                return false;
+        for(int i = 0; i<s.size(); i++){
+            if(frq.count(s[i])){
+                frq[s[i]]++;
+            }else{
+                frq[s[i]] = 1;
             }
-            count[t[i] - 'a']--;
         }
-        return true;
+       for(int i = 0; i<t.size(); i++){
+        if(frq.count(t[i])){
+            frq[t[i]]--;
+            if(frq[t[i]] == 0){
+                frq.erase(t[i]);
+            }
+        }else{
+            return false;
+        }
+    }
+
+        return frq.size() == 0;
     }
 };
